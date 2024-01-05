1c8dee959daca10b030dea0a9ff2156618ad24c1
KapitaenKirSche <benjamin@marx-merten.de>
2024-01-05 20:01:33 +0100

    leveldesign change

--- config.py
+++ config.py
@@ -407,7 +407,7 @@ level07={
     "size"            : 100,
     "size_in_between" : 10,
     "gamemode"        : "maxTile",
-    "maxTile"         : 512, #only, if gamemode == "maxTile
+    "maxTile"         : 256, #only, if gamemode == "maxTile
     "level_text"      : "Erreiche das 512 Tile. Vorsicht: Gegner! (Blau!)",
     "chance_enemy_spawn": 10,
     "chance_duplicate_spawn": 0,
